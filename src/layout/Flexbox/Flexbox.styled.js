fd5536a1a50d149123b54648931bf46608a6a449
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-03-23 17:59:33 +0100

    Finnish styling Flexbox comp

--- src/layout/Flexbox/Flexbox.styled.js
+++ src/layout/Flexbox/Flexbox.styled.js
@@ -2,6 +2,30 @@ import styled from 'styled-components';
 
 const Flexbox = styled.div`
   display: flex;
+
+  flex-wrap: ${props => {
+    if(props.wrap) return props.wrap
+    return 'wrap'
+  }};
+
+  justify-content: ${props => {
+    if(props.justifyContent) return props.justifyContent
+    return 'center'
+  }};
+
+  align-items: ${props => {
+    if(props.alignItems) return props.alignItems
+    return 'center'
+  }};
+
+  align-content: ${props => {
+    if(props.alignContent) return props.alignContent
+    return 'center'
+  }};
+
+  flex-direction: ${props => (
+    props.column ? 'column' : 'row'
+  )};
 `;
 
 export default Flexbox;
