e9639ba4824b7591eac51032ab078d2287b0463d
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-03-24 17:32:35 +0100

    Create smoke test for Container

--- src/layout/Container/Container.test.js
+++ src/layout/Container/Container.test.js
@@ -1,2 +1,17 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
+
+import Container from './Container';
+
+describe('<Container />', () => {
+  it('Renders without crashing', () => {
+    const children = <div>Test Container</div>;
+
+    shallow(
+      <Container>
+        {children}
+      </Container>
+    );
+  })
+});
+
