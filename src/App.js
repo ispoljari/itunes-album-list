61411dc5504c07e7eeaa6f587b52e9057babeb81
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-03-26 17:48:35 +0100

    Implement data fetching from API

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import GlobalStyle from './Global.styled';
 import { Container } from './layout';
 import { Header, Main } from './components';
 
+const URL = 'https://itunes.apple.com/us/rss/topalbums/limit=10/json';
+
 class App extends Component {
   state= {
     template: {
@@ -12,11 +14,41 @@ class App extends Component {
       imgSrc: 'https://via.placeholder.com/170',
       artist: "Artist Name",
       price: '6.99$'
+    },
+    loading: false,
+    data: null
+  }
+
+  loadAlbums = async () => {
+    try {
+      this.setState({
+        loading: true,
+        error: false
+      });
+
+      const rawData = await fetch(URL);
+      const data = await rawData.json();
+
+      this.setState({
+        loading: false,
+        data
+      });
+    } catch (error) {
+      this.setState({
+        loading: false,
+        error: true
+      });
     }
   }
 
+  componentDidMount() {
+    this.loadAlbums();
+  }
+
   render() {
-    const { template } = this.state;
+    const { template, loading, data } = this.state;
+    console.log(loading);
+    console.log(data);
 
     return (
       <React.Fragment>
