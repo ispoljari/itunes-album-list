54e10701d61d78c35997acbee574f99240fab6e1
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-03-26 17:59:37 +0100

    Add styles to spinner

---
+++ src/components/Spinner/Spinner.styled.js
@@ -0,0 +1,22 @@
+import styled, { keyframes } from 'styled-components';
+
+const spin = keyframes`
+  0% { 
+    transform: rotate(0deg);
+  }
+
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+const loader = styled.div`
+  border: 16px solid #f3f3f3;
+  border-top: 16px solid #3498db;
+  border-radius: 50%;
+  width: 120px;
+  height: 120px;
+  animation: ${spin} 2s linear infinite;
+`;
+
+export default loader;
