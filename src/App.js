d304a8d71a7c9afe58c0a2f23b92d8c9dd5d09b7
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-03-28 19:41:42 +0100

    Move App comp logic to AppContainer

--- src/App.js
+++ src/App.js
@@ -4,61 +4,8 @@ import { Normalize } from 'styled-normalize';
 import GlobalStyle from './Global.styled';
 import { Container } from './layout';
 import { Header, Main, ScrollToTop } from './components';
-import { fetchData } from './common';
 
 class App extends Component {
-  state= {
-    loading: false,
-    limit: 0,
-    albums: {},
-    inputValue: '',
-    error: null
-  };
-
-  loadAlbums = () => {
-    this.setState(prevState => ({
-      loading: true,
-      error: null,
-      limit: prevState.limit + 10
-    }), () => {
-      this.loadResultsFromAPI();
-    })
-  };
-
-
-  loadResultsFromAPI = async () => {
-    const URL = `https://itunes.apple.com/us/rss/topalbums/limit=${this.state.limit}/json`;
-    const results = await fetchData(URL);
-
-    if(!(results instanceof Error)) {
-      this.setState({
-        albums: {
-          ...this.state.albums,
-          ...results.feed.entry
-        },
-        loading: false
-      });
-    } else {
-      this.setState({
-        error: results,
-        loading: false
-      });
-    }
-  };
-
-  componentDidMount() {
-    this.loadAlbums();
-  };
-
-  handleChange = e => {
-    const rawInput = e.target.value;
-    const inputValue = rawInput.toLowerCase();
-
-    this.setState({
-      inputValue
-    });
-  };
-
   render() {
     const { loading, albums, error, limit, inputValue } = this.state;
 
