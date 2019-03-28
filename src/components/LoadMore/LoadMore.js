9da640dcb9d7e519288a3c4e9a2758e99a19a420
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-03-28 20:27:34 +0100

    Refactor LoadMore comp from class to function

--- src/components/LoadMore/LoadMore.js
+++ src/components/LoadMore/LoadMore.js
@@ -1,30 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import { Column, Box } from '../../layout';
 import { Button } from './LoadMore.styled';
 
-class LoadMore extends Component {
-  render() {
-    const { onClick } = this.props;
-
-    return(
-      <Column>
-        <Box
-          dsPlay="flex"
-          justifyContent="center"
-        >
-          <Button
-            type="button"
-            onClick={onClick}
-          >
-            Load more albums
-          </Button>
-        </Box>
-      </Column>
-    )
-  }
-};
+const LoadMore = ({ onClick }) => (
+  <Column>
+    <Box
+      dsPlay="flex"
+      justifyContent="center"
+    >
+      <Button
+        type="button"
+        onClick={onClick}
+      >
+        Load more albums
+      </Button>
+    </Box>
+  </Column>
+);
 
 LoadMore.propTypes = {
   onClick: PropTypes.func.isRequired
